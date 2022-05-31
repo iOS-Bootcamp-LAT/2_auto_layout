48591ec98b345a87978c689b9f328a3836118169
David Granado J <david.granado@interfase.com.bo>
2022-05-31 08:22:58 -0400

    Initial Commit

---
+++ 2_auto_layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_auto_layout
+//
+//  Created by David Granado Jordan on 5/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
